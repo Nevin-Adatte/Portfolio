3186bcd3dbc37d4a0c2720d01049dcb741270062
Nevin V Adatte <nevinvadatte@gmail.com>
2023-02-02 21:57:39 +0530

    Footer section completed

--- script.js
+++ script.js
@@ -5,11 +5,36 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing-1",{
+        strings: ["Student.", "Broadcaster.", "Designer."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Student.", "Broadcaster.", "Designer."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 });
